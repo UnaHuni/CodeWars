5d82ba064a212d9b27ca72a21f7c228478207ad3
Una Miladinovic <unahuni@gmail.com>
2022-05-22 00:26:27 +0200

    8 kyu To square(root) or not to square(root)

---
+++ CodeWars0005/js/main.js
@@ -0,0 +1,13 @@
+//To square(root) or not to square(root)
+
+// Write a method, that will get an integer array as parameter and will process
+// every number from this array.
+// Return a new array with processing every number of the input-array like this:
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+
+function squareOrSquareRoot(array) {
+    return array.map(element => Number.isInteger(Math.sqrt(element)) ? Math.sqrt(element) : (element * element))
+}
